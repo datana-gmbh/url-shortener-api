7925960304ec4ea3ee8d14ab81e719e91796dfa3
Oskar Stark <oskarstark@googlemail.com>
2023-05-25 19:49:26 +0200

    Enhancement: Add `FakeUrlShortenerApi` (#2)

---
+++ src/FakeUrlShortenerApi.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of datana-gmbh/url-shortener-api.
+ *
+ * (c) Datana GmbH <info@datana.rocks>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Datana\UrlShortener\Api;
+
+use Datana\UrlShortener\Api\Response\UrlShortenerResponse;
+
+final class FakeUrlShortenerApi implements UrlShortenerApiInterface
+{
+    public function generateShortUrl(string $targetUrl): UrlShortenerResponse
+    {
+        return new UrlShortenerResponse([
+            'short_url' => 'https://example.de/'.md5($targetUrl),
+        ]);
+    }
+}
